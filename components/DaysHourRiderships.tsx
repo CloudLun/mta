15217826cf7dacd7a5082ce711a88a9e3cc12396
CloudLun <cloud830902@gmail.com>
2023-11-16 09:42:21 -0500

    daily riderships labels

--- components/DaysHourRiderships.tsx
+++ components/DaysHourRiderships.tsx
@@ -32,6 +32,7 @@ const DaysHourRiderships = () => {
   const gPromise = d3.csv("/data/g_line_day_hour_ridership.csv")
   const jPromise = d3.csv("/data/j_line_day_hour_ridership.csv")
 
+  const bandwidth = 50.0826
 
 
   useEffect(() => {
@@ -63,46 +64,110 @@ const DaysHourRiderships = () => {
 
       }
 
+      console.log(x.bandwidth())
 
 
 
-      const highlight = "#eee"
+
+      const highlight = "rgba(50, 50, 50, .3)"
 
 
       days.forEach((d, i) => {
 
         if (i < 5) {
-          d3.select(daysRef[i].current)
-            .append('rect')
-                          // @ts-ignore
-            .attr("x", x("5") - 0.1 * x.bandwidth())
-            .attr("y", y(17500000))
-            .attr("width", x.bandwidth() * 6.2)
-            .attr("height", height)
-            .attr("fill", highlight)
-            .attr("opacity", 1)
 
           d3.select(daysRef[i].current)
-            .append('rect')
-                          // @ts-ignore
-            .attr("x", x("14") - 0.1 * x.bandwidth())
-            .attr("y", y(17500000))
-            .attr("width", x.bandwidth() * 6.2)
-            .attr("height", height)
-            .attr("fill", highlight)
-            .attr("opacity", 1)
+            .append("line")
+            .style("stroke", highlight)
+            .style("stroke-width", 1.25)
+            .style("stroke-dasharray", ("3, 3"))
+            .attr("x1", x("6"))
+            .attr("y1", 10)
+            .attr("x2", x("6"))
+            .attr("y2", height);
+
+          d3.select(daysRef[i].current)
+            .append("line")
+            .style("stroke", highlight)
+            .style("stroke-width", 1.25)
+            .style("stroke-dasharray", ("3, 3"))
+            .attr("x1", x("10") + 1 * x.bandwidth())
+            .attr("y1", 10)
+            .attr("x2", x("10") + 1 * x.bandwidth())
+            .attr("y2", height);
+
+          d3.select(daysRef[i].current)
+            .append("line")
+            .style("stroke", highlight)
+            .style("stroke-width", 1.25)
+            .style("stroke-dasharray", ("3, 3"))
+            .attr("x1", x("15"))
+            .attr("y1", 10)
+            .attr("x2", x("15"))
+            .attr("y2", height);
+
+          d3.select(daysRef[i].current)
+            .append("line")
+            .style("stroke", highlight)
+            .style("stroke-width", 1.25)
+            .style("stroke-dasharray", ("3, 3"))
+            .attr("x1", x("18") + 1 * x.bandwidth())
+            .attr("y1", 10)
+            .attr("x2", x("18") + 1 * x.bandwidth())
+            .attr("y2", height);
+
+          // d3.select(daysRef[i].current)
+          //   .append('rect')
+          //   // @ts-ignore
+          //   .attr("x", x("5") - 0.1 * x.bandwidth())
+          //   .attr("y", y(17500000))
+          //   .attr("width", x.bandwidth() * 6.2)
+          //   .attr("height", height)
+          //   .attr("fill", highlight)
+          //   .attr("opacity", 1)
+
+          // d3.select(daysRef[i].current)
+          //   .append('rect')
+          //   // @ts-ignore
+          //   .attr("x", x("14") - 0.1 * x.bandwidth())
+          //   .attr("y", y(17500000))
+          //   .attr("width", x.bandwidth() * 6.2)
+          //   .attr("height", height)
+          //   .attr("fill", highlight)
+          //   .attr("opacity", 1)
         }
 
         if (i > 4) {
+
           d3.select(daysRef[i].current)
-            .append('rect')
-                          // @ts-ignore
-            .attr("x", x("10") - 0.1 * x.bandwidth())
-            .attr("y", y(17500000))
-            .attr("width", x.bandwidth() * 10)
-            .attr("height", height)
-            .attr("fill", highlight)
-            .attr("opacity", 1)
+            .append("line")
+            .style("stroke", highlight)
+            .style("stroke-width", 1.25)
+            .style("stroke-dasharray", ("3, 3"))
+            .attr("x1", x("6"))
+            .attr("y1", 10)
+            .attr("x2", x("6"))
+            .attr("y2", height);
+
+          d3.select(daysRef[i].current)
+            .append("line")
+            .style("stroke", highlight)
+            .style("stroke-width", 1.25)
+            .style("stroke-dasharray", ("3, 3"))
+            .attr("x1", x("18") + 1 * x.bandwidth())
+            .attr("y1", 10)
+            .attr("x2", x("18") + 1 * x.bandwidth())
+            .attr("y2", height);
+
+          // d3.select(daysRef[i].current)
+          //   .append('rect')
+          //   // @ts-ignore
+          //   .attr("x", x("10") - 0.1 * x.bandwidth())
+          //   .attr("y", y(17500000))
+          //   .attr("width", x.bandwidth() * 10)
+          //   .attr("height", height)
+          //   .attr("fill", highlight)
+          //   .attr("opacity", 1)
         }
 
 
@@ -111,7 +176,7 @@ const DaysHourRiderships = () => {
           .data(datasets[0])
           .enter()
           .append("rect")
-                        // @ts-ignore
+          // @ts-ignore
           .attr("x", d => x(d.hour))
           .attr("y", d => y(+d[days[i]]))
           .attr("width", x.bandwidth())
@@ -127,12 +192,27 @@ const DaysHourRiderships = () => {
 
   return (
     <div className='m-auto p-[20px] w-[90vw] h-[100vh]'>
-      <div>Total Hours Ridership By Days</div>
+      <div className='flex justify-between items-end  h-[8vh]'>
+        <div>
+          <div className='font-semibold text-[30px] text-[#0039A6]'>Daily Total Hours Ridership By Hours</div>
+          <div className='mt-[3px] mb-[5px] font-regular text-[#323232]'>Distributions of total daily riderships in NYC devided by hours</div>
+        </div>
+        <div className='flex items-center gap-[5px]'>
+          <div className='w-[20px] h-[10px] bg-[#0039A6]'></div>
+          <div className='my-[5px] font-regular text-[12px] text-[#323232]'>Riderships</div>
+        </div>
+
+
+      </div>
       {
         days.map((d, i) => {
           return (
-            <div key={i} className='relative w-full h-[calc((90vh)/7)]'>
-              <div className='absolute bottom-[-30px] left-[10px] font-semibold text-[#0039A6]'>{d}</div>
+            <div key={i} className=' relative mt-[5px] w-full h-[calc((80vh)/7)]'>
+              <div className='absolute bottom-[-40px] left-[10px] font-semibold text-[#0039A6]'>{d}</div>
+              <div className={`absolute bottom-[-16px] left-[25px] font-semibold text-[10px] text-[#323232]`}>0:00</div>
+              <div className={`absolute bottom-[-16px] left-[400px] font-semibold text-[10px] text-[#323232]`}>6:00</div>
+              <div className={`absolute bottom-[-16px]  left-[775px] font-semibold text-[10px] text-[#323232]`}>12:00</div>
+              <div className={`absolute bottom-[-16px] left-[1150px] font-semibold text-[10px] text-[#323232]`}>18:00</div>
               <svg className=' w-full h-full ' ref={daysRef[i]}></svg>
             </div>
 
